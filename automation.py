e4740c02d4c8cceb7276bfa7d1b108f62201edc7
Niraj Verma <nirajv@login03.cm.cluster>
2017-10-25 10:14:10 -0500

    Modified module

--- automation.py
+++ automation.py
@@ -1,20 +1,23 @@
 import os
-from module import zmatrix 
-
-file='test5.xyz'
-filename=file.split('.')
+import module 
+import sys
+if len(sys.argv)<2:
+	file=raw_input('Enter file path : ')
+else:
+	file=sys.argv[1]
+filename=file.split('/')[-1].split('.')[0]
 
 s0='%chk='+filename+'.chk\n'
 
 s1="""%nprocshared=64
 %mem=39GB
-#p opt=z-matrix freq b3lyp/6-31g(d,p) nosymm 
-empiricaldispersion=gd3bj int=ultrafine
+#p opt=z-matrix freq b3lyp/6-31g(d,p) nosymm empiricaldispersion=gd3bj int=ultrafine
+
 """
 
-s2=filename+'\n0 1\n'
+s2=filename+'\n\n0 1\n'
 
-s3=zmatrix.string(file)
+s3=module.zmatrix(file)
 
 f=open(filename+'.g16','w')
 f.write(s0)
@@ -23,4 +26,4 @@ f.write(s2)
 f.write(s3)
 f.write('\n')
 
-
+f.close()
--- module.py
+++ module.py
@@ -1,49 +1,63 @@
 import os
-def search_deep(n_path,func,args=[],ref=1):
-        if 'Trash' in n_path:
-		return
-	try:
-        for i in os.listdir(n_path):
-            if os.path.isdir(n_path+'/'+i):
-                search_deep(n_path+'/'+i,func,args,ref)
-            else:
-				if ref:
-                    for j in args:
-                        if j==i[-len(j):]:
-                        	func(n_path+'/'+i)
-				else:
-					for j in args:
-                        if j in i:
-                            func(n_path+'/'+i)
-        except OSError:
-                 func(n_path)
 
 def zmatrix(file):
     os.system('module load gaussian/g16a')
     filename=file.split('.')[0]+'.com'
-    os.systme('newzmat -ixyz -Ozmat -rebuildzmat '+file+' '+filename)
+    os.system('newzmat -ixyz -Ozmat -rebuildzmat '+file+' '+filename)
     f=open(filename,'r')
     lines=f.readlines()
     f.close()
-    ref=1
+    ref=0
     string=''
     var=0
     count=-1
+    li=[]
+    d,dr={},0
+    for i in range (len(lines)):
+	if len(lines[i].strip().split())==0:
+		if ref==0:
+                        continue
+                elif ref==1:
+                        break
+        if 'C'==lines[i].split()[0]:
+            ref=1
+        elif ref==1:
+            line=lines[i].strip().split(',')
+            if len(line)>=7:
+                li.append(line[2])
+                li.append(line[4])
+                li.append(line[6])
+	    elif len(line)>=5:
+                li.append(line[2])
+                li.append(line[4])
+            elif len(line)>=3:
+                li.append(line[2])
+            if 'Variables:' in lines[i]:
+                dr=1
+	    if dr==1 or dr==2:
+		dr+=1
+	    if dr==3:
+		a,b=lines[i].strip().split('=')
+		d[a]=b
     for line in lines:
+	if len(line.strip().split())==0:
+		if ref==1:
+			continue
+		elif ref==0:
+			break
         if 'C'==line.strip().split()[0]:
             ref=0
         if ref==0:
-            string+=' '.join(line.strip().split(','))+'\n'
             if 'Variables:' in line:
-                var=1
-            if var==1:
-                count+=1
-            if count==6:
-                var=0
-                count=0
-                string+=' Constants:\n'
-        if len(line.strip().split())==0:
-            break
+                break
+	    string+=' '.join(line.strip().split(','))+'\n'
+
+    li=['='.join([r,d[r]]) for r in li]
+    string+='\n Variables:\n'
+    li1=li[:6]
+    string+='\n'.join(li1)
+    string+='\n Constants:\n'
+    string+='\n'.join(li[6:])+'\n'
     return string 
 
 
