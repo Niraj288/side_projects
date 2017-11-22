4dc825ed4635308df1194948f41c575abb12506b
Verma <47510753@18-11512.fondrenscience.smu.edu>
2017-11-21 18:44:42 -0600

    made fchktoxyz

--- hbond_out.py
+++ hbond_out.py
@@ -7,19 +7,26 @@ import make_lmode_file
 import addKaTopdb
 
 #give com file
-def xyz(filename):
-	com2xyz.make_xyz(filename+'.com')
+def xyz(filename,end):
+	com2xyz.make_xyz(filename+'.'+end)
 
 def hbonds(filename):
 	try:
+		print 'Using pdb ...' 
 		f=open(filename+'.pdb','r')
 		f.close()
 		try:
 			pdb.job(filename+'.pdb',filename+'.xyz')
+			print 'Success using pdb .'
 		except KeyError:
+			print 'Mismatch of heavy atoms in pdb and com or fchk file !!'
+			print 'Using xyz file ...'
 			pdb1.job(filename+'.xyz')
 	except IOError:
+		print 'No pdb file found !'
+		print 'Using xyz file ...'
 		pdb1.job(filename+'.xyz')
+		print 'Success using xyz.'
 
 def lmode(filename):
 	make_lmode_file.make_alm(filename+'.txt','_ah')
@@ -29,10 +36,15 @@ def lmode(filename):
 
 def job():
 	path=sys.argv[1]
-	filename=path.split('/')[-1].split('.')[0]
-	xyz(filename)
+	li=path.split('/')[-1].split('.')
+	filename=li[0]
+	print 'Making xyz ...'
+	xyz(filename,li[1])
+	print 'Calculating H-Bonds ...'
 	hbonds(filename)
+	print 'Calculating local modes ...'
 	lmode(filename)
+	print 'Done!!'
 
 job()
 
