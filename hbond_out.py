c9d3fb8e6de3b5636270e3b853d26bc294c2fc47
Verma <47510753@18-11512.fondrenscience.smu.edu>
2018-09-07 11:31:39 -0500

    excel made for hbond

--- hbond_out.py
+++ hbond_out.py
@@ -13,6 +13,67 @@ import addBCP
 import addFreq
 import addLPCont
 import addCharges
+import xlwt
+
+def make_excel(d,filename):
+    workbook = xlwt.Workbook()
+    #workbook = xl_copy(rb)
+    name='sheet1'
+    sheet = workbook.add_sheet(name)
+
+    sheet.write(0,0,'S/No')
+    sheet.write(0,1,'Bond')
+    sheet.write(0,2,'atomIDs')
+    sheet.write(0,3,'HB length')
+
+    for i in d['hb']:
+        sheet.write(int(i[:-1]),0,i[:-1])
+        sheet.write(int(i[:-1]),1,d['hb'][i][0]) 
+        sheet.write(int(i[:-1]),2,d['hb'][i][1])
+        sheet.write(int(i[:-1]),3,float(d['hb'][i][2]))
+
+    ref=3
+    for i in d:
+    	if i=='hb':
+    		continue
+    	if i=='l':
+    		ref+=1
+    		sheet.write(0,ref,'Ka(Acceptor)')
+    		sheet.write(0,ref+1,'Ka(Donor)')
+    		a,b=d[i]
+    		for j in a:
+    			sheet.write(int(j[:-1]),ref,a[j])
+    		for j in b:
+    			sheet.write(int(j[:-1]),ref+1,b[j])
+    		ref+=1
+    	if i=='lf':
+    		ref+=1
+    		sheet.write(0,ref,'freq(Acceptor)')
+    		sheet.write(0,ref+1,'freq(Donor)')
+    		a,b=d[i]
+    		for j in a:
+    			sheet.write(int(j[:-1]),ref,a[j])
+    		for j in b:
+    			sheet.write(int(j[:-1]),ref+1,b[j])
+    		ref+=1
+    	if i=='bcp':
+    		ref+=1
+    		sheet.write(0,ref,'K(r)atBCP')
+    		for j in d[i]:
+    			sheet.write(int(j[:-1]),ref,d[i][j])
+    	if i=='charg':
+    		ref+=1
+    		sheet.write(0,ref,'MullikenCharges')
+    		for j in d[i]:
+    			sheet.write(int(j[:-1]),ref,d[i][j])
+    	if i=='C':
+    		ref+=1
+    		sheet.write(0,ref,'Charges(NBO)')
+    		for j in d[i]:
+    			sheet.write(int(j[:-1]),ref,d[i][j])
+
+
+    workbook.save(filename+'.xls')
 
 #give com file
 def xyz(filename,end):
@@ -25,25 +86,28 @@ def hbonds(filename):
 	if '-c' in sys.argv:
 		print 'Using xyz file ...'
 		print 'Considering C-H...O bonds ...'
-		pdb_c.job(filename+'.xyz')
+		p=pdb_c.job(filename+'.xyz')
 		print 'Success using xyz.'
-		return
+		return p
 	try:
 		print 'Using pdb ...' 
 		f=open(filename+'.pdb','r')
 		f.close()
 		try:
-			pdb.job(filename+'.pdb',filename+'.xyz')
+			p=pdb.job(filename+'.pdb',filename+'.xyz')
 			print 'Success using pdb .'
+			return p
 		except KeyError:
 			print 'Mismatch of heavy atoms in pdb and com or fchk file !!'
 			print 'Using xyz file ...'
-			pdb1.job(filename+'.xyz')
+			p=pdb1.job(filename+'.xyz')
+			return p
 	except IOError:
 		print 'No pdb file found !'
 		print 'Using xyz file ...'
-		pdb1.job(filename+'.xyz')
+		p=pdb1.job(filename+'.xyz')
 		print 'Success using xyz.'
+		return p 
 
 def lmode(filename,software='gaussian'):
 	if software=='xtb':
@@ -54,9 +118,10 @@ def lmode(filename,software='gaussian'):
 		addKaTopdb.addKa(filename+'.txt','_dh')
 	else:
 		make_lmode_file.make_alm(filename+'.txt','_ah')
-		addKaTopdb.addKa(filename+'.txt','_ah')
+		a=addKaTopdb.addKa(filename+'.txt','_ah')
 		make_lmode_file.make_alm(filename+'.txt','_dh')
-		addKaTopdb.addKa(filename+'.txt','_dh')
+		d=addKaTopdb.addKa(filename+'.txt','_dh')
+		return [a,d]
 
 def lmodefreq(filename,software='gaussian'):
 	# change things for xtb
@@ -70,13 +135,15 @@ def lmodefreq(filename,software='gaussian'):
 		if '-l' not in sys.argv:
 			make_lmode_file.make_alm(filename+'.txt','_ah')
 			make_lmode_file.make_alm(filename+'.txt','_dh')
-		addFreq.addFreq(filename+'.txt','_ah')
-		addFreq.addFreq(filename+'.txt','_dh')
+		a=addFreq.addFreq(filename+'.txt','_ah')
+		d=addFreq.addFreq(filename+'.txt','_dh')
+		return [a,d]
 
 def BCP(filename):
-	addBCP.job(filename)
+	return addBCP.job(filename)
 
 def job(path):
+	lis_excel={}
 	li=path.split('/')[-1].split('.')
 	if len(li)>1:
 		filename='.'.join(li[:-1])
@@ -87,8 +154,9 @@ def job(path):
 		xyz(filename,li[-1])
 		print 'Calculating H-Bonds ...'
 		curr=time.time()
-		hbonds(filename)
+		hb=hbonds(filename)
 		print 'Calculation done in',time.time()-curr,'seconds'
+		lis_excel['hb']=hb
 	else:
 		print 'Adding elements to .txt ...'
 	
@@ -97,26 +165,34 @@ def job(path):
 		lmode(filename,'xtb')
 	elif '-l' in sys.argv:
 		print 'Calculating local modes ...'
-		lmode(filename)
+		l=lmode(filename)
+		lis_excel['l']=l
 	if '-f' in sys.argv:
 		print 'Calculating local modes frequencies ...'
-		lmodefreq(filename)
+		lf=lmodefreq(filename)
+		lis_excel['lf']=lf
 
 	if '-d' in sys.argv:
 		print 'Fetching electron density at BCP (a.u) ...'
-		BCP(filename)
+		bcp=BCP(filename)
+		lis_excel['bcp']=bcp
 
 	if '-LP' in sys.argv:
 		print 'Adding all lone pair contribution to BD* of H ...'
-		addLPCont.job(filename+'.txt')
+		lp=addLPCont.job(filename+'.txt')
+		lis_excel['lp']=lp
 
 	if '-Charg' in sys.argv:
 		print 'Adding Acceptor and Hydrogen charges ...'
-		addCharges.job(filename+'.fchk')
+		charg=addCharges.job(filename+'.fchk')
+		lis_excel['charg']=charg
 
 	if '-C' in sys.argv:
 		print 'Adding Acceptor and Hydrogen charges from NBO calculations ...'
-		addC.job(filename+'.g09.out')
+		C=addC.job(filename+'.g09.out')
+		lis_excel['C']=C
+
+	make_excel(lis_excel,filename)
 
 	print 'Done!!'
 
